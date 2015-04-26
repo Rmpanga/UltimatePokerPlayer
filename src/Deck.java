15ccddd551b2423c27121ba24a17153e68f8819b
Albert <albertung@gmail.com>
2015-04-26 19:05:02 -0400

    updating card class

--- src/Deck.java
+++ src/Deck.java
@@ -1,8 +1,19 @@
 
 public class Deck {
 	
-	public Deck() {
+	public static final String[] possible_cards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+	public static final String[] possible_suits = {"clubs", "hearts", "spades", "diamonds"};
+	
+	public void createDeck() {
 		
 	}
+	
+//	public Deck() {
+//		for(possible_cards) {
+//			for(possible_suits) {
+//				new Card();
+//			}
+//		}
+//	}
 	
 }
