dff3368558ef5b7e923cb9399b4bb1b1a6378694
Albert <albertung@gmail.com>
2015-04-26 18:56:44 -0400

    Adding to Card class

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,18 @@
 
 public class Card {
-
+	private String value;
+	private String suit;
+	
+	public Card(String value, String suit) {
+		this.value = value;
+		this.suit = suit;
+	}
+	
+	public String retValue() {
+		return value;
+	}
+	
+	public String retSuit() {
+		return suit;
+	}
 }
