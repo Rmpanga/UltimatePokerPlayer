a36ef655c0bd3471e2a5c9f17c321e38a32e68ec
Richard Mpanga <rmpanga@umass.edu>
2015-04-26 20:57:57 -0400

    Updated computer 	-addHand Method

--- src/Computer.java
+++ src/Computer.java
@@ -2,8 +2,13 @@
 public class Computer {
 	
 	private String name;
-	
+	public Hand hand;
 	public Computer(String name) {
 		this.name = name;
 	}
+	
+	public void addHand(Hand hand){
+		this.hand = hand;
+	}
+	
 }
