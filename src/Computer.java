c171773088498268de7e0cf8cf2b7c30bc8bbe7c
Albert <albertung@gmail.com>
2015-04-26 20:28:43 -0400

    update computer and user class

--- src/Computer.java
+++ src/Computer.java
@@ -1,9 +1,13 @@
 
 public class Computer {
-	
+	private Hand computer_hand;
 	private String name;
 	
 	public Computer(String name) {
 		this.name = name;
+	}
+	
+	public void addHand(Hand hand) {
+		computer_hand = hand;
 	}
 }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,14 @@
 
 public class User {
-
+	private Hand user_hand;
+	private String name;
+	
+	public User(String name) {
+		this.name = name;
+	}
+	
+	public void addHand(Hand hand) {
+		user_hand = hand;
+	}
+	
 }
