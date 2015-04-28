1915450a96a7493ce0ad096c8d178535d74acb5c
Richard Mpanga <rmpanga@umass.edu>
2015-04-27 20:56:24 -0400

    Added Shuffle Logic to the Deck class Reformatted

--- src/Deck.java
+++ src/Deck.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Deck {
 	
 	public static final String[] possible_cards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 	public static final String[] possible_suits = {"clubs", "hearts", "spades", "diamonds"};
+	
+
+	
 	static ArrayList<Card> deck = new ArrayList<Card>(52);
 	
 	/*@Tested
@@ -18,38 +22,69 @@ public class Deck {
 			}
 		}
 	}
+	
+	/*@Tested
+	 * Shuffles Deck randomly
+	 */
+	
+	public static void shuffleDeck(){
+		ArrayList<Integer> indexes = new ArrayList<Integer>(52);
+		ArrayList<Card> shuffledDeck = new ArrayList<Card>(52);
+		Random r = new Random ();
+		
+		for (int x =0; x<deck.size(); x++){
+		//	
+			//shuffledDeck.add(null);
+			indexes.add(x);
+		}
+		
+		
+		for (int j =0; j<52; j++){
+			//ystem.out.println("J: " + j);
+			boolean redo = true;
+			int rValue = r.nextInt(indexes.size());
+			shuffledDeck.add(deck.get(rValue));
+			indexes.remove(rValue);
+			deck.remove(rValue);
+		
+		}
+		deck = shuffledDeck;
+	}
+	
+	/**                                                                          DEBUG SECTION                                                                   **/
+	
+	
 
-	/*
+	/*@Debug
 	 * 
 	 */
 	public static void printDeck(){
-		
+		int count = 0;
 		for (Card card : deck){
 			
-			System.out.println(card.retValue() +"," + card.retSuit());
+			System.out.println(card.retValue() +"," + card.retSuit() +" " + count);
+		
 			
+			count++;
 		}
 		
 	}
 	
-	/*
+
+	
+	/*@Debug
 	 * 
 	 */
-	public static void shuffleDeck(){
-		
-		
-	}
-	
-	
-	
 	
 	public static void main ( String [] args){
 		
 		populateDeck();
-		printDeck();
 		
 		System.out.println();
-			
+		System.out.println("Shuffling the deck & Printing");
+		shuffleDeck();
+		printDeck();
+		
 	}
 
 //	public Deck() {
